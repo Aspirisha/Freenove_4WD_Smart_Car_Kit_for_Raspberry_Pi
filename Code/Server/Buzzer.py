0040cfc32f3b5683274c0f5c04bfc1caf838ca86
Andrei Serebro <aspirisha@gmail.com>
2025-08-27 23:09:40 +0100

    refactor Buzzer.py

--- Code/Server/Buzzer.py
+++ Code/Server/Buzzer.py
@@ -1,19 +1,23 @@
 import time
 from gpiozero import Buzzer
 from Command import COMMAND as cmd
-buzzer = Buzzer(17)
+
+
+BUZZER_PIN = 17
+
+buzzer = Buzzer(pin=BUZZER_PIN)
+
+
 class Buzzer:
-    def run(self,command):
-        if command!="0":
+    def run(self, command):
+        if command != "0":
             buzzer.on()
         else:
             buzzer.off()
-if __name__=='__main__':
-    B=Buzzer()
-    B.run('1')
+
+
+if __name__ == "__main__":
+    B = Buzzer()
+    B.run("1")
     time.sleep(3)
-    B.run('0')
-
-
-
-
+    B.run("0")
